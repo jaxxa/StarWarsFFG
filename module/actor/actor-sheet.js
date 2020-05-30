b121cfbea5f42e0852d4a8c05a8e8ca022839da5
Chris Stadther <c.stadther@gmail.com>
2020-05-30 16:20:21 -0400

    Changed default tab to characteristics and skills.

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -11,7 +11,7 @@ export class swffgActorSheet extends ActorSheet {
       template: "systems/starwars-ffg/templates/actor/character-sheet.html",
       width: 600,
       height: 600,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "characteristics" }]
     });
   }
 
